07570b1a9c460fdf8980c854c73ae1e1e927ebed
Sirojiddin <siroj.origin.profile>
2023-10-14 08:06:35 +0900

    feat: routerni qurish

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("Starting Web Server");
 const express = require("express");
 const app = express();
+const router = require("./router");
 
 // Connect/call MongoDB
 const db = require("./server").db();
@@ -18,5 +19,6 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing codes
+app.use("/", router);
 
 module.exports = app;
