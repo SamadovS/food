0a0343dec2479e86038904e983d47aecc2f19b5f
Sirojiddin <siroj.origin.profile>
2023-10-14 10:09:10 +0900

    fix: building mongoose connection and member schema model

---
+++ schema/member.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const memberSchema = new mongoose.Schema({
+    mb_nick: {
+        type: String,
+        required: true,
+        index: { unique: true, sparse: true },
+    },
+    mb_phone: {
+        type: String,
+        required: true,
+    },
+    mb_password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    mb_type: {
+        type: String,
+        required: false,
+        default: "USER",
+        enum: {
+            values: member_type_enums,
+            message: "{VALUES} is not among permitted values",
+        },
+    },
+    mb_status: {
+        type: String,
+        required: false,
+        default: "ACTIVE",
+        enum: {
+            values: member_status_enums,
+            message: "{VALUES} is not among permitted values",
+        },
+    },
+    mb_full_name: {
+        type: String,
+        required: false,
+    },
+    mb_address: {
+        type: String,
+        required: false,
+    },
+    mb_image: {
+        type: String,
+        required: false,
+    },
+    mb_point: {
+        type: String,
+        required: false,
+        default: 0,
+    },
+});
