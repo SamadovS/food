59d16f4c4a36e27d4aba7636f559dc14944fb86c
Sirojiddin <siroj.origin.profile>
2023-10-23 09:15:03 +0900

    feat: develop Product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -71,6 +71,17 @@ restaurantController.logoutProcess = (req, res) => {
     res.send("You are in Logout Page");
 };
 
+restaurantController.validateAuthRestaurant = async (req, res, next) => {
+    if (req.session?.member?.mb_type === "RESTAURANT") {
+        req.member = req.session.member;
+        next();
+    } else
+        res.json({
+            state: "fail",
+            message: "only authenticated members with restaurant type",
+        });
+};
+
 restaurantController.checkSession = (req, res) => {
     if (req.session?.member) {
         res.json({ state: "succeed", data: req.session.member });
--- router_bssr.js
+++ router_bssr.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const app = express();
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/productController");
 
 /**********************************
  *          BSSR EJS              *
@@ -19,5 +20,11 @@ router_bssr.get("/logout", restaurantController.logoutProcess);
 router_bssr.get("/check-me", restaurantController.checkSession);
 
 router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.post(
+    "/products/create",
+    restaurantController.validateAuthRestaurant,
+    productController.addNewProduct
+);
+router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
 
 module.exports = router_bssr;
